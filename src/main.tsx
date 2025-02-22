5c5df073c1ddb6bf9fec2f7853a816f47ed7b9da
lauri865 <lauri.lehtmaa@gmail.com>
2025-02-22 19:10:16 +0100

    u

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Demo from './Demo.tsx';
-import './global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
